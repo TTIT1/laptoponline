6cfa77cb5ab5172e1b5ca8733d0a542bbaca1d45
Nguyễn Thế Trường TechFusion <thetruongit1236@gmail.com>
2025-09-18 11:27:40 +0700

    fixx login

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,3 @@
 INSERT INTO users (username, password, email, role)
-VALUES ('admin', 'admin123', 'admin@example.com', 'ADMIN')
-ON CONFLICT (username) DO UPDATE
-SET password = EXCLUDED.password,
-    email = EXCLUDED.email,
-    role = EXCLUDED.role;
+VALUES ('admin', '{noop}admin123', 'admin@example.com', 'ADMIN')
+ON CONFLICT (username) DO NOTHING;
